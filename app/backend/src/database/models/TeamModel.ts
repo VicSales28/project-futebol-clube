b6cfc92840917a691275127901cfec5dfdaa34aa
Victoria Sales <contatovictoriasales@gmail.com>
2023-08-07 13:20:47 -0300

    Configuração inicial

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,36 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Team extends Model<InferAttributes<Team>,
+InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+
+export default Team;
