95c6e2ac7e85af1613b70d36d5769f23fa47a6cc
Victoria Sales <contatovictoriasales@gmail.com>
2023-08-09 13:49:04 -0300

    Correção de erro em AuthMiddleware

--- app/backend/src/routers/auth.router.ts
+++ app/backend/src/routers/auth.router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AuthController from '../controllers/auth.controller';
-import AuthMiddleware from '../middlewares/ auth.middleware';
+import AuthMiddleware from '../middlewares/auth.middleware';
 
 const authRouter = Router();
 
