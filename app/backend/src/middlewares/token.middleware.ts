2c3920a8c02dcba6818e88fb897c32cceccf8422
Victoria Sales <contatovictoriasales@gmail.com>
2023-08-14 16:38:34 -0300

    Adiciona função para obter token sem prefixo Bearer

--- app/backend/src/middlewares/token.middleware.ts
+++ app/backend/src/middlewares/token.middleware.ts
@@ -2,6 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import JWTUtility from '../utils/JWTUtility';
 
 class TokenMiddleware {
+  // Função auxiliar para obter o token sem o prefixo "Bearer"
+  private static getAccessToken = (tokenWithBearer: string) => {
+    const splitted = tokenWithBearer.split(' ');
+    return splitted[splitted.length - 1];
+  };
+
   static checkToken = (req: Request, res: Response, next: NextFunction) => {
     try {
       const tokenWithBearer = req.header('authorization');
@@ -13,7 +19,10 @@ class TokenMiddleware {
       }
 
       const JWT = new JWTUtility();
-      const verified = JWT.verify(tokenWithBearer);
+
+      const token = TokenMiddleware.getAccessToken(tokenWithBearer);
+
+      const verified = JWT.verify(token);
 
       res.locals.user = verified;
 
