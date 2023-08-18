418a28893e3c3b4e56fa0f92bc5861c23b978a54
Victoria Sales <contatovictoriasales@gmail.com>
2023-08-18 12:22:05 -0300

    Desenvolve testes para a rota GET /leaderboard/home

---
+++ app/backend/src/tests/Leaderboard.test.ts
@@ -0,0 +1,22 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { homeLeaderboard } from './mocks/Leaderboard.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testa rota GET /leaderboard/home', () => {
+  afterEach(sinon.restore);
+
+  it('Testa retorno com as informações do desempenho dos times da casa', async () => {
+    const { status, body } = await chai.request(app).get('/leaderboard/home');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(homeLeaderboard);
+  });
+});
